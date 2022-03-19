3e3a75802904d0bb1dee746ef876c79d80b97d4c
Your Name <you@example.com>
2022-03-19 15:05:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# MP3 to MP4
+
+## Features
+音声ファイルと静止画1枚から動画を作成する
+
+## Requirement
+- python 3.x
+- ffmpeg n4.4-19-g8d172d9409
+
+## Installation
+
+```bash
+pip install mutagen
+```
+
+## Usage
+input_audioフォルダにmp4化したいファイルを配置して下記コマンドを実行する
+
+
+```
+python script.py
+```
+
+生成されたmp4ファイルはoutputディレクトリに出力される
+
+## Refrenrace
+- [FFmpegでの音声ファイルと静止画1枚からの動画の作成方法](https://dev.classmethod.jp/articles/ffmpeg-create-movie-by-audio/)
---
+++ script.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+import mutagen
+from mutagen.easyid3 import EasyID3
+
+audio_file_dir = os.path.join(os.getcwd(), 'input_audio')
+output_file_dir = os.path.join(os.getcwd(), 'output')
+
+background_img = os.path.join(os.getcwd(), 'background.png')
+# audioファイル（mp3）ファイル一覧取得
+files = os.listdir(audio_file_dir)
+audio_files = [f for f in files if (os.path.isfile(os.path.join(audio_file_dir, f)) and 'mp3' in f)]
+print(audio_files)
+for audio_file in audio_files:
+    try:
+        audio_file_path = os.path.join(audio_file_dir, audio_file)
+        tags = EasyID3(audio_file_path)
+        items = tags.items()
+        album = items[0][1][0]
+        title = items[1][1][0]
+    except mutagen.id3._util.ID3NoHeaderError:
+        # mp3メタ情報が設定されていない場合のハンドリング
+        album = ''
+        title = audio_file.replace('.mp3', '')
+
+    out_file_name = f'{album}_{title}.mp4'
+    out_file_path = os.path.join(output_file_dir, out_file_name)
+    print(album, title)
+
+    command = f'ffmpeg -loop 1 -r 30000/1001 -i "{background_img}" -i "{audio_file_path}" -acodec aac -strict experimental -ab 320k -ac 2 -ar 48000 -pix_fmt yuv420p -shortest "{out_file_path}"'
+    print(command)
+    subprocess.run(command)
+
+    # https://qiita.com/moshi/items/57735f2602aa0cc3b88e
+    # https://dev.classmethod.jp/articles/ffmpeg-create-movie-by-audio/
